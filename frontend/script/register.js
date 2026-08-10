32d3c937d97e2c4e457b6e06d216055ff3899bbf
Marijke Van den Eede <115033271+MarijkeVdE@users.noreply.github.com>
2026-08-10 19:22:15 +0200

    register.html and register.js improved

---
+++ frontend/script/register.js
@@ -0,0 +1,13 @@
+const form = document.getElementById("register-form");
+
+form.addEventListener("submit", async (event) => {
+
+    event.preventDefault();
+
+    // gegevens ophalen
+
+    // fetch()
+
+    // antwoord verwerken
+
+});
